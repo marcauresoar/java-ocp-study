c97ccc4bd5c282802d5c999229bcd0f3eec77f3f
Marco Aurelio Soares <maurelio@avenuecode.com>
2017-12-28 12:22:35 -0200

    more enum examples

--- src/chapter1/enums/EnumExamples.java
+++ src/chapter1/enums/EnumExamples.java
@@ -3,6 +3,7 @@ package chapter1.enums;
 public class EnumExamples {
     public static void main(String... args){
         Season1.SUMMER.printExpectedVisitors(); // high
+        Season1.WINTER.printExpectedVisitors(); // Winter Low
         System.out.println(Season2.SPRING.getSeasonName()); // Spring
     }
 }
--- src/chapter1/enums/Season1.java
+++ src/chapter1/enums/Season1.java
@@ -1,7 +1,11 @@
 package chapter1.enums;
 
 public enum Season1 {
-    WINTER("Low"), SPRING("Medium"), SUMMER("High"), FALL("Medium");
+    WINTER("Low") {
+        public void printExpectedVisitors(){
+            System.out.println("Winter Low");
+        }
+    }, SPRING("Medium"), SUMMER("High"), FALL("Medium");
     private String expectedVisitors;
     Season1(String expectedVisitors){ // private constructor redundant
         this.expectedVisitors = expectedVisitors;
