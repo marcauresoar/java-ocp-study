3ff18e620a52e692e2fe3e16650f2eaa6dda4082
Marco <marcauresoar@gmail.com>
2018-05-01 10:17:40 -0300

    adding ObjectStream and PrintWriter examples

---
+++ src/chapter8/PrintWriterSample.java
@@ -0,0 +1,21 @@
+package chapter8;
+
+import java.io.*;
+
+public class PrintWriterSample {
+    public static void main(String[] args) throws IOException {
+        File source = new File("zoo.log");
+        try (PrintWriter out = new PrintWriter(
+                new BufferedWriter(
+                        new FileWriter(source)))) {
+            out.print("Today's weather is: ");
+            out.println("Sunny");
+            out.print("TOday's temperature at the zoo is: ");
+            out.print(1/3.0);
+            out.println('C');
+            out.format("It has rained 10.12 inches this year");
+            out.println();
+            out.printf("It may rain 21.2 more inches this year");
+        }
+    }
+}
